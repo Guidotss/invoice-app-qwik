fb0942b03fe2d79d14811e3411cdb248327118f3
Guidotss <guidoolguin@hotmail.com>
2023-12-06 17:53:11 -0300

    Agregando fuente

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors:  {
+        'charcoal-blue'   : '#252945',
+        'deep-purple'     : '#141625',
+        'light-coral'     : '#EC5757',
+        'light-gray'      : '#F8F8FB',
+        'light-purple'    : '#9277FF',
+        'light-tomato'    : '#9277FF',
+        'manatee'         : '#888EB0',
+        'midnight-black'  : '#0C0E16',
+        'midnight-blue'   : '#1E2139',
+        'periwinkle'      : '#DFE3FA',
+        'purple'          : '#7C5DFA',
+        'soft-periwinkle' : '#7E88C3',
+      },
+    },
   },
   plugins: [],
 };
